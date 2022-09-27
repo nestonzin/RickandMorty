005ffc8b3c62861851f7675593f1b77bbcb7f1f8
nestonzin <dinestoor@outlook.com>
2022-09-27 13:54:19 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import { Hero } from "./components/Hero";
-import { Navbar } from "./components/Navbar";
-import { useQuery } from "@tanstack/react-query";
-import { Spinner, Box, Flex } from "@chakra-ui/react";
-
-const api = {
-  base: "https://rickandmortyapi.com/api/character",
-};
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
-  const { isLoading, error, data } = useQuery(["rmData"], () =>
-    fetch(api.base).then((res) => res.json())
-  );
-
-  const results = data?.results?.slice(0, 35);
-
-  // console.log(data.results);
-
   return (
-    <Flex alignItems={"center"} flexDirection={"column"}>
-      {isLoading ? <Spinner /> : Hero}
-      <Navbar />
-      <Hero results={results} />
-    </Flex>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,25 @@
 import { DefaultLayout } from "../../_layouts/default";
+import { Hero } from "../../components/Hero";
+import { useQuery } from "@tanstack/react-query";
+import { Spinner, Box, Flex } from "@chakra-ui/react";
 
+
+
+const api = {
+  base: "https://rickandmortyapi.com/api/character",
+};
 
 export const Home = () => {
-    return <DefaultLayout>
+  const { isLoading, error, data } = useQuery(["rmData"], () =>
+    fetch(api.base).then((res) => res.json())
+  );
 
+  const results = data?.results?.slice(0, 35);
+
+  // console.log(data.results);
+  return (
+    <DefaultLayout>
+      <Hero results={results} />
     </DefaultLayout>
+  );
 };
